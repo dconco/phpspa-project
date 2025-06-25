cd2b81ec7967ba2abdeb2fcdcb5189073242f31c
Dave Conco <118613296+dconco@users.noreply.github.com>
2025-06-25 14:38:36 +0000

    added code to homepage

--- src/pages/HomePage.php
+++ src/pages/HomePage.php
@@ -3,5 +3,12 @@
 use phpSPA\Component;
 
 return (new Component(function () {
-    $name = 'dconco'''''''''''''''''''''''''''''''''''''''''''''''''''''''
-}));
+    $name = 'dconco';
+
+    return <<<HTML
+        <h2>Home Page</h2>
+        <p>Welcome to the home page, $name!</p>
+    HTML;
+}))
+  ->title('Home Page')
+  ->route('/');
